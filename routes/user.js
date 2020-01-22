5df44a4c00bc0350987c90fb18dcaec2118b999f
siddhartha_tal <siddhartha@touchalife.org>
2020-01-22 10:49:35 +0530

    added code

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,7 @@ function ensureAuthenticated(req, res, next) {
         return next();
     } else {
         //req.flash('error_msg','You are not logged in');
-        res.redirect('/noauth');
+        res.redirect('api/noauth');
     }
 }
 
