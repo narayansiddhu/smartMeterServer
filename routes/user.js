6dcc53937b27a5e085d0f3ea9bda636b6656f031
Siddhartha Narayanadasu <sid@Siddharthas-MacBook-Air.local>
2020-01-22 09:58:14 +0530

    added

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const authController = require("../controllers/auth");
 const { UserController } = require('../controllers');
 
 
 router.post('/register', UserController.registerUser);
 
-
+router.get('/getAlluers', authController.isFacebookCallback,function(req, res){
+    res.send("This the Auth Data")
+});
 
 module.exports = router;
