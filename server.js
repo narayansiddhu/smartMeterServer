75b04876c79b6f3b007cb44eb31d17d05f4de325
siddhartha_tal <siddhartha@touchalife.org>
2020-01-22 12:28:48 +0530

    added code

--- server.js
+++ server.js
@@ -19,10 +19,15 @@ require('express-async-errors');
 // Express Session
 
 
-
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
   
 // Passport init
 app.use(passport.initialize());
+app.use(passport.session());
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
@@ -30,11 +35,7 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // Express Session
-app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
-}));
+
 
 app.use(cors());
 app.use(methodOverride());
