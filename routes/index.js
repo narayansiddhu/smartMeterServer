a053323f313ebc3260fd30818cc529e565ce9a2d
Siddhartha Narayanadasu <sid@Siddharthas-MacBook-Air.local>
2020-01-23 02:48:36 +0530

    added

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const Donation = require('./Donation');
 const Profile = require('./Profile');
 const Request = require('./Request');
-const UserDetails = require('./User');
+const User = require('./User');
 
 module.exports = function (app) {
-    app.use('/api', UserDetails)
+    app.use('/api', User)
     app.use('/api', Donation);
     app.use('/api', Profile);
     app.use('/api', Request);
